0965d1a43c5272fac701afeaa57573866e16c60c
master-of-fire <99187679+master-of-fire@users.noreply.github.com>
2025-02-11 10:14:17 -0500

    Update Comments.java

---
+++ Comments.java
@@ -0,0 +1,16 @@
+/************************************
+*Project: Comments
+*Programmer: Zaid Ahmad
+*Date: Feb 11, 2025
+*Program Name: Comments.java
+*************************************/
+
+
+
+//message to user
+
+//assignment
+
+/* If you want to write a detailed coment and it wraps 
+around the screen, use this block style of commenting
+*/
